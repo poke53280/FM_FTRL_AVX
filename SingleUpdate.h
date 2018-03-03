352ba1293fc94cbd51926c1a6708abc22859d3f9
poke53280 <anders.topper@gmail.com>
2018-03-03 15:31:24 +0100

    Updates to compile in C

--- SingleUpdate.h
+++ SingleUpdate.h
@@ -2,18 +2,8 @@
 #ifndef _SINGLEUPDATE_H_
 #define _SINGLEUPDATE_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-  void update_single_EXT(const int* inds, double* vals, int lenn, const double e, double ialpha, double* w, double* z, double* n,
-    double alpha_fm, const double L2_fm, double* w_fm, double* z_fm, double* n_fm, int D_fm, bool bias_term, int nThreads);
-
-  int doubler_EXT(int d);
-
-#ifdef __cplusplus
-}
-#endif
+void update_single_EXT(const int* inds, double* vals, int lenn, const double e, double ialpha, double* w, double* z, double* n,
+    double alpha_fm, const double L2_fm, double* w_fm, double* z_fm, double* n_fm, int D_fm, int bias_term, int nThreads);
 
 #endif   // _SINGLEUPDATE_H_
 
